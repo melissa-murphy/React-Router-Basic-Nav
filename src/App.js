255ea019cc08a6fd3cb6a3415f624a5b4818e591
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-06 13:26:38 -0700

    functional links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    
+
+    <Router />
   </div>
 );
 
